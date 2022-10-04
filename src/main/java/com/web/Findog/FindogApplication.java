0cff33bac3d57617e007168fa4717f1e9af31ac2
qazazx20 <qazazx20@daum.net>
2022-10-05 06:20:06 +0900

    프로젝트 생성

---
+++ src/main/java/com/web/Findog/FindogApplication.java
@@ -0,0 +1,13 @@
+package com.web.Findog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/Findog/FindogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.Findog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
